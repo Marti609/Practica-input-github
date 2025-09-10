4531629cb9faaa3a0d21b20d8058a265b68d28f6
Marti609 <marti.galman@campus.xaloc.org>
2025-09-10 09:46:39 +0200

    confirmación

---
+++ prueba input.py
@@ -0,0 +1,14 @@
+# Pedimos al usuario que ingrese un número
+numero1 = input("Introduce el primer número: ")
+
+# Pedimos el segundo número
+numero2 = input("Introduce el segundo número: ")
+
+# Convertimos las entradas que son cadenas a enteros (asumiendo que son números enteros)
+num1 = int(numero1)
+num2 = int(numero2)
+
+resultado = num1 + num2
+
+print("La suma es:", resultado)
+
