438ab463538d59dfe9593bb9500dedd07aa2cd37
Marti609 <marti.galman@campus.xaloc.org>
2025-10-28 10:46:18 +0100

    PYTHON

---
+++ PASSWORD VERSION 1/Actividad.password.py
@@ -0,0 +1,18 @@
+print("INSTRUCCIONS")
+print("1.La longitud del password ha de tenir entre 6 i 8 caràcters")
+print("Forçar els següents valors segons la posició indicada:")
+"Posició 0: un numero major o igual que 1 i menor o igual que 5" 
+print("Posició 1: una lletra minúscula")
+print("Posició 2: una lletra majúscula ")
+print("Posició 3: un dels següents símbols *, _, @")
+print("Posició 4: una lletra minúscula")
+print("Posició 5: un numero major o igual que 6 i menor o igual que 9")
+print("Posició 6: un dels següents símbols &, /, #")
+print("Posició 7: un numero menor o igual que 5 ")  
+password = input("Introdueix la paraula clau: ")
+
+#comprobem si la password te entre 6-8 carácters
+longitud_password = len(password)
+if longitud_password>8 or longitud_password<6:
+    print(f"Error, el password te una longitud de {longitud_password} i no cumpleix els requisits")
+elif (password[0]) 
---
+++ SUMATIVA INTERMEDIA/Ejercicio 3.py
@@ -0,0 +1,6 @@
+import math
+radio=int(input("introduce el radio: "))
+altura=int(input("introduce la altura: "))
+formato= input("quiere que el resultado se muestre en mayusculas o en minusculas, escriba mayusculas o minusculas")
+volumen=math.pi*radio**altura
+if formato==
