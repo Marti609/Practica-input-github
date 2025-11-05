a8852b1886fedad5e3ed0b71ce87493668f3746d
Marti609 <marti.galman@campus.xaloc.org>
2025-11-05 09:54:15 +0100

    Python

---
+++ BUCLES/Actividad guiada 1.py
@@ -0,0 +1,5 @@
+password="holaquetal"
+total=0
+#repeticiones=int(input("número de veces: "))
+for j in range (len(password)):
+    print(password[j])
---
+++ BUCLES/Actividad guiada 2.py
@@ -0,0 +1,9 @@
+password="a1e4jk"
+vocales="aeiouAEIOU"
+total=0
+#repeticiones int(input("numero de veces: "))
+for i in password:
+    if i.isnumeric: 
+        total=total+int(i)
+    if i in vocales
+print(total)
