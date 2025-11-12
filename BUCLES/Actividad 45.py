3ca256491757e069889acb06436e56a5ccdedd4a
Marti609 <marti.galman@campus.xaloc.org>
2025-11-12 09:53:38 +0100

    python

---
+++ BUCLES/Actividad 45.py
@@ -0,0 +1,15 @@
+# 45. Realiza un programa que permita introducir una palabra por teclado y puedas recorrer el stringdistinguiendo vocales y las consonantes:
+palabra=input("Introduce una palabra: ")
+vocales = 'aeiouáéíóú'
+
+lista_consonantes=[]
+lista_vocales=[]
+
+for letra in palabra:
+    if letra in vocales:
+        lista_vocales.append(letra)
+    else:
+        lista_consonantes.append(letra)
+
+print(f"las consonantes de la palabra {palabra} son: {lista_consonantes}")
+print(f"las vocales de la palabra {palabra} son: {lista_vocales}")
