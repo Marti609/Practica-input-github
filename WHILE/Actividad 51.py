c4f393e9f3c9b1570aaf4965a64758d9e3526d54
Marti609 <marti.galman@campus.xaloc.org>
2025-11-19 09:53:22 +0100

    python

---
+++ WHILE/Actividad 51.py
@@ -0,0 +1,8 @@
+# 51. A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el número de veces que desea que repita la frase Buenos días. Con While
+veces=int(input("Introduce un numero: "))
+
+contador=0
+
+while contador<veces:
+    print("Buenos dias")
+    contador+=1
