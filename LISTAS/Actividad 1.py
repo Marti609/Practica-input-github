cbeb76d39f9e75efa6f6095fe1a943ee86c6973e
Marti609 <marti.galman@campus.xaloc.org>
2026-01-09 15:58:32 +0100

    PYTHON

---
+++ LISTAS/Actividad 1.py
@@ -0,0 +1,12 @@
+milista=[1,2,3,4,5]
+milistapor2:[]
+maximo=max(milista)
+minimo=min(milista)
+suma=sum(milista)
+milistapor2= [i* 2 for i in milista]
+
+print(milista)
+print(f"maximo:{maximo}")
+print(f"minimo:{minimo}")
+print(f"suma total:{suma}")
+print(milistapor2)
